cfcc1ce598cb24b6dabad7f37ab1959afa4e1dad
Param Sampat <paramsampat@Params-MacBook-Air.local>
2025-02-04 18:39:34 -0500

    Updates

---
+++ precache-manifest.6677bbf25e41fb9dc4de62b7700cec5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb3e38d90a92bbcf060d1a3f89f8322",
+    "url": "/params-poker-tracker/index.html"
+  },
+  {
+    "revision": "765140fcf2e57fe3d7a6",
+    "url": "/params-poker-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "64cf12bd42063b781084",
+    "url": "/params-poker-tracker/static/js/2.05245c5c.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/params-poker-tracker/static/js/2.05245c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3877b27bcdc60ce930b3",
+    "url": "/params-poker-tracker/static/js/3.162c8b78.chunk.js"
+  },
+  {
+    "revision": "765140fcf2e57fe3d7a6",
+    "url": "/params-poker-tracker/static/js/main.b44d9c89.chunk.js"
+  },
+  {
+    "revision": "c146537bfbdc08c0ad5a",
+    "url": "/params-poker-tracker/static/js/runtime-main.e1f89a03.js"
+  }
+]);
